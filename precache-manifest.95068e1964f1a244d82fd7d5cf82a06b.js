52523ddc6ea4f72036200a644acebd8d26f2706b
syam sunder <syam@macsof.com>
2020-06-09 21:19:26 +0530

    Updates

---
+++ precache-manifest.95068e1964f1a244d82fd7d5cf82a06b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0fbd7e01f794fbfc61f262444f9ea1b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "769bf6095a981a3a7389",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/my-app/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769bf6095a981a3a7389",
+    "url": "/my-app/static/js/main.c0d3b57e.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
